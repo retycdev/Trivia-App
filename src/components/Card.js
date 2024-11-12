af1561a6b5e5758adbf2c62b6a8f1bc21ed479ee
dan_kibaara <kibaara.gamedev@gmail.com>
2024-11-12 14:49:14 +0300

    Adds changes to Cards, n package.json

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,9 +5,11 @@ import styled from 'styled-components'
 const Container=styled.div`
 max-width:640px;
 width:100%;
-height:196px;
+height:256px;
 border-radius:8px;
 cursor:pointer;
+@media(max-width:768px){
+height:196px;}
 `
 const Image=styled.img`
 height:100%;
@@ -16,7 +18,7 @@ object-fit:cover;
 border-radius:inherit;
 `
 const Card = ({img
-    
+
 }) => {
   return (
     <div>
