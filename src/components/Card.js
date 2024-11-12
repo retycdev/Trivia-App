21a9bf7878e7d7d1f21bb30a95494886c16e7b40
dan_kibaara <kibaara.gamedev@gmail.com>
2024-11-12 14:01:11 +0300

    Adds Card component

---
+++ src/components/Card.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+const Container=styled.div`
+max-width:640px;
+width:100%;
+height:196px;
+border-radius:8px;
+cursor:pointer;
+`
+const Image=styled.img`
+height:100%;
+width:100%;
+object-fit:cover;
+border-radius:inherit;
+`
+const Card = ({img}) => {
+  return (
+    <div>
+        <Container>
+            <Image src={img}/>
+        </Container>
+      
+    </div>
+  )
+}
+
+export default Card
