27102484d241172364f048b72d07e7f9e8fa5950
dan_kibaara <kibaara.gamedev@gmail.com>
2024-11-12 14:09:14 +0300

    Improvises CardList

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -4,14 +4,18 @@ import styled from 'styled-components'
 const Container=styled.div`
 height:calc(100vh - 64px);
 overflow-y:auto;
+display:grid;
+grid-template-columns:repeat(3, 1fr);
 padding:48px 64px;
 
 @media(max-width:480px){
 padding:48px 12px;
+grid-template-columns:1fr;
 }
 
 @media(min-width:481px) and (max-width:1024px){
 padding:48px 32px;
+grid-template-columns:repeat(2,1fr);
 
 }
 `
