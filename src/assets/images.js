34ba025cd6e552fafc5392d403219c48f052280e
dan_kibaara <kibaara.gamedev@gmail.com>
2024-11-12 14:20:15 +0300

    Adds images in assets folder

---
+++ src/assets/images.js
@@ -0,0 +1,10 @@
+const backgrndImgs=[
+    {src:"https://miro.medium.com/v2/resize:fit:1200/1*LyZcwuLWv2FArOumCxobpA.png"},
+    {src:"https://api.journey.skillreactor.io/media/CourseImages%2FHTML%2FLesson%201%20-%20Overview%20Of%20The%20Web%20And%20HTML%2F1.1%20What%20Is%20HTML/image/9f3b7d87-4563-4e9a-b676-445727f18b74.jpg"},
+    {src:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUH-oIAvcVSDEN54K4XFbmUAynW9xva-dQ0g&s"},
+    {src:"https://blog.vtutor.com/wp-content/uploads/2019/12/Linux.jpg"},
+    {src:"https://bashlogo.com/img/logo/jpg/full_colored_light.jpg"},
+    {src:"https://cdn.prod.website-files.com/661dc153b706dbb6ca6be504/663b222726c7f9b37d3c6d05_react.png"},
+]
+
+export default backgrndImgs;
