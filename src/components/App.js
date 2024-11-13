82267058e26b9918c183be302947635fdd40d28c
dan_kibaara <kibaara.gamedev@gmail.com>
2024-11-14 00:34:08 +0300

    Adds HashRouter

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import CardList from "./CardList";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Playroom from "./Playroom";
 
 
